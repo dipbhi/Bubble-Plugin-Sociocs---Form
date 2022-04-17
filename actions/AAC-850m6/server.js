e8b99bb767be93c19bc596af01479afefd1bfc3a
Dipen Bhikadya <dipen.bhikadya@gmail.com>
2022-04-17 23:09:40 +0530

    Automatic commit from Bubble

--- actions/AAC-850m6/server.js
+++ actions/AAC-850m6/server.js
@@ -1,9 +1,44 @@
 function(properties, context) {
 
 	const fetch = require('node-fetch');
+    
+    if(!context.keys["API URL"]){
+        return "API URL is missing in the plugin configuration.";
+    }
+    
+    if(!context.keys["SECRET KEY"]){
+        return "SECRET KEY is missing in the plugin configuration.";
+    }
+    
+    const body = properties.form_fields.reduce((prev, current) => {
+        prev[current.key] = current.value;
+        return prev;
+    }, {});
+
+    if(!body.email) {
+        return "email key is required.";
+    }
+    
 
     return context.async(cb => {
-        return cb(null, {status: true, result: ""});
+        fetch(context.keys["API URL"], {
+            method: 'post',
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json",
+                "apikey": context.keys["SECRET KEY"],
+            }
+        }).then(res => {
+            return res.json();
+        }).then(response => {
+            if(response.status === "success") {
+                return cb(undefined, response);
+            } else {
+                return cb(response.error);
+            }
+        }).catch(err => {
+            return cb(err);
+        });
     });
 
 }
